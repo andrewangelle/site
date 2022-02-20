bb92df982bcb6fc4410de9c8acfdc7f9564a80e9
Andrew Angelle <andrewangelle@gmail.com>
2022-02-20 18:11:13 -0500

    get data from resource route

--- app/routes/portfolio.tsx
+++ app/routes/portfolio.tsx
@@ -1,10 +1,12 @@
+import { Fragment } from 'react';
 import { Link, LoaderFunction, useLoaderData } from 'remix';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import * as Icons from '@fortawesome/free-brands-svg-icons'
+import { PortfolioCard } from '~/components/PortfolioCard';
 
-import { Card, Wrapper, SplitLeft, SplitRight, NavItem, NameWrapper, NavList } from '~/styles';
+import {  Wrapper, SplitLeft, SplitRight, NavItem, NameWrapper, NavList } from '~/styles';
 
-type PortfolioData = {
+const portfolioDataPath = 'http://andrewangelle.com/resources/portfolio';
+
+export type PortfolioData = {
   title: string;
   description: string;
   githubLink: string;
@@ -13,29 +15,9 @@ type PortfolioData = {
 }
 
 export const loader: LoaderFunction = async args => {
-  return [
-    {
-      title: 'React Glide',
-      description: 'a multi purpose carousel module built in react',
-      githubLink: 'http://www.github.com/andrewangelle/react-glide',
-      demoLink: 'https://codesandbox.io/s/r7166733lm',
-      demoText: 'Demo'
-    },
-    {
-      title: 'React PayPal Button',
-      description: 'A button component to implement PayPal\'s Express Checkout in React',
-      githubLink: 'http://www.github.com/andrewangelle/react-paypal-button',
-      demoLink: '',
-      demoText: ''
-    },
-    {
-      title: 'Websynth',
-      description: 'A synthesizer built as a webapp using the Web Audio API',
-      githubLink: 'https://github.com/andrewangelle/websynth',
-      demoLink: '',
-      demoText: '' 
-    }
-  ]
+  const response = await fetch(portfolioDataPath);
+  const data = await response.json();
+  return data
 }
 
 export default function Portfolio(){
@@ -44,41 +26,14 @@ export default function Portfolio(){
     <Wrapper>
       <SplitLeft>
         <NameWrapper>
-          <section >
-            {data.map((portfolioData, index) => {
-              const { title, description, githubLink, demoLink, demoText } = portfolioData;
-              return (
-                <>
-                  {index === 0 && <hr />}
-                  <Card>
-                    <h3>{title}</h3>
-                    <p>{description}</p>
-                    <footer>
-                      <a
-                        href={githubLink}
-                        target="_blank"
-                        style={{ color: 'inherit' }}
-                      >
-                        <FontAwesomeIcon
-                          icon={Icons.faGithub}
-                          size="2x"
-                        />
-                      </a>
-                      {demoLink && (
-                        <a
-                          href={demoLink}
-                          target="_blank"
-                          style={{ color: 'inherit' }}
-                        >
-                          <button>{demoText}</button>
-                        </a>
-                      )}
-                    </footer>
-                  </Card>
-                  <hr />
-                </>
-              )
-            })}
+          <section>
+            {data.map((portfolioData, index) => (
+              <Fragment key={index}>
+                {index === 0 && <hr />}
+                  <PortfolioCard {...portfolioData} />
+                <hr />
+              </Fragment>
+            ))}
           </section>
         </NameWrapper>
       </SplitLeft>
