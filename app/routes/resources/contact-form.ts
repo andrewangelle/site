1b008705640d26f406edc0a029b51016b8541c76
Andrew Angelle <andrewangelle@gmail.com>
2022-02-27 14:48:56 -0500

    reformat the messag

--- app/routes/resources/contact-form.ts
+++ app/routes/resources/contact-form.ts
@@ -5,8 +5,8 @@ export async function action({request}: {request: Request}){
   const data = await request.json();
   const msg = {
     to: 'andrewangelle@gmail.com', // Change to your recipient
-    from: `websiteMessage-${data.name}@andrewangelle.com`, // Change to your verified sender
-    subject: data.subject,
+    from: `websiteMessage@andrewangelle.com`, // Change to your verified sender
+    subject: `${data.name}-${data.subject}`,
     text: data.message
   }
   sendgrid.setApiKey(process.env.SEND_GRID_API_KEY!)
