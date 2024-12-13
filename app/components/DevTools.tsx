84a53f3781eb3f118c99fea5cb934ad1df111e8f
Andrew Angelle <andrewangelle@gmail.com>
2024-12-13 18:40:14 -0500

    upgrade to react 19

--- app/components/DevTools.tsx
+++ app/components/DevTools.tsx
@@ -4,11 +4,8 @@ const TanStackRouterDevtools =
   process.env.NODE_ENV === 'production'
     ? () => null // Render nothing in production
     : lazy(() =>
-        // Lazy load in development
         import('@tanstack/router-devtools').then((res) => ({
-          default: res.TanStackRouterDevtools,
-          // For Embedded Mode
-          // default: res.TanStackRouterDevtoolsPanel
+          default: res.TanStackRouterDevtools
         })),
       )
 
