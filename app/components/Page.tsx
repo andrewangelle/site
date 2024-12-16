ccd2f3286a7e4aa6c65d81bcde988123d9257ef9
Andrew Angelle <andrewangelle@gmail.com>
2024-12-16 10:55:14 -0500

    Fix position bug on name for wide screen widths

--- app/components/Page.tsx
+++ app/components/Page.tsx
@@ -35,9 +35,10 @@ export function Page() {
   }
 
   function getLeftOffset(value: number) {
+    const result = Math.ceil(value * 1000);
     const positionOffset = isMobile ? 10 : 30;
-    const result = Math.ceil(value * 1000 + positionOffset);
-    return `-${result}px`;
+    const offset = isMobile && result < 1000 ? positionOffset : 1000;
+    return `-${result + offset}px`;
   }
 
   function getRightOffset(value: number) {
