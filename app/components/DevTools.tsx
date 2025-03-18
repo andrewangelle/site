617bdde319e4209977ead129e149ccef99629c07
Andrew Angelle <andrewangelle@gmail.com>
2025-03-18 08:48:33 -0400

    chore: Update tsr versions

--- app/components/DevTools.tsx
+++ app/components/DevTools.tsx
@@ -4,7 +4,7 @@ const TanStackRouterDevtools =
   process.env.NODE_ENV === 'production'
     ? () => null // Render nothing in production
     : lazy(() =>
-        import('@tanstack/router-devtools').then((res) => ({
+        import('@tanstack/react-router-devtools').then((res) => ({
           default: res.TanStackRouterDevtools,
         })),
       );
