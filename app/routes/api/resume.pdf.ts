301505b717b9dcfbef3cc39eba7ee75c84ffb7b6
Andrew Angelle <andrewangelle@gmail.com>
2025-04-27 10:38:19 -0400

    fix: move pdf file to public

--- app/routes/api/resume.pdf.ts
+++ app/routes/api/resume.pdf.ts
@@ -1,23 +1,18 @@
-import { readFile, readdir } from 'node:fs/promises';
-import { join } from 'node:path';
 import { createAPIFileRoute } from '@tanstack/react-start/api';
 
 export const APIRoute = createAPIFileRoute('/api/resume/pdf')({
   GET: async ({ request }) => {
     try {
-      const pathToPdf = join(process.cwd(), './app/utils/resume.pdf');
-      const file = await readFile(pathToPdf);
-      return new Response(file, {
+      const url = new URL(request.url);
+      const response = await fetch(`${url.origin}/resume.pdf`);
+      return new Response(response.body, {
         headers: getResponseHeaders(request),
       });
     } catch (e) {
-      const err = e as Error;
-      const files = await readdir(process.cwd(), { recursive: true });
       return new Response(
         JSON.stringify({
           status: 500,
-          files: files,
-          message: err.message,
+          message: (e as Error).message,
         }),
       );
     }
