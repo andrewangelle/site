8ba3e0e538d00f8a34c32fd677ce2829a85f2b13
Andrew Angelle <andrewangelle@gmail.com>
2025-05-14 10:11:53 -0400

    fix: fix focus management

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -13,35 +13,7 @@ import linksSectionCss from '~/styles/links-container.css?url';
 import nameSectionCss from '~/styles/name-container.css?url';
 
 export const Route = createRootRoute({
-  head: () => ({
-    meta: [
-      {
-        charSet: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: 'Andrew Angelle',
-      },
-      {
-        rel: 'icon',
-        link: '/favicon.ico',
-      },
-    ],
-    links: [
-      { rel: 'stylesheet', href: indexCss },
-      { rel: 'stylesheet', href: nameSectionCss },
-      { rel: 'stylesheet', href: linksSectionCss },
-      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: '' },
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap',
-      },
-    ],
-  }),
+  head: getDocumentHead,
   component: Root,
 });
 
@@ -68,4 +40,36 @@ function RootDocument({ children }: { children: ReactNode }) {
       </Provider>
     </html>
   );
+}
+
+function getDocumentHead() {
+  return {
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'Andrew Angelle',
+      },
+      {
+        rel: 'icon',
+        link: '/favicon.ico',
+      },
+    ],
+    links: [
+      { rel: 'stylesheet', href: indexCss },
+      { rel: 'stylesheet', href: nameSectionCss },
+      { rel: 'stylesheet', href: linksSectionCss },
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap',
+      },
+    ],
+  };
 }
