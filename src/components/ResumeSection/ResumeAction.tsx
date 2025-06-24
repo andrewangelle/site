0d29ac901fa53a5c252c2ac31322f8f88d3b6287
Andrew Angelle <andrewangelle@gmail.com>
2025-06-24 19:16:29 -0400

    bugs

--- src/components/ResumeSection/ResumeAction.tsx
+++ src/components/ResumeSection/ResumeAction.tsx
@@ -1,5 +1,8 @@
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import { useNavigate } from '@tanstack/react-router';
 import { useSetAtom } from 'jotai';
 import { motion } from 'motion/react';
+import { ResumePDF } from '~/PDF/ResumePDF';
 import { ViewOrDownload } from '~/components/ResumeSection/ViewOrDownload';
 import { SECTIONS, activeLinkAtom, activeViewAtom } from '~/store/atoms';
 import { enterExitAnimationProps, strings } from '~/utils/constants';
@@ -7,12 +10,13 @@ import { enterExitAnimationProps, strings } from '~/utils/constants';
 export type ResumeActionLinkProps = { action: 'view' | 'download' };
 
 export function ResumeAction({ action }: ResumeActionLinkProps) {
+  const navigate = useNavigate();
   const setActiveLink = useSetAtom(activeLinkAtom);
   const setActiveView = useSetAtom(activeViewAtom);
-
   function viewOrDownload() {
-    const query = action === 'download' ? '?action=download' : '';
-    window?.location.assign(`/api/resume/pdf${query}`);
+    if (action === 'view') {
+      navigate({ to: '/resume' });
+    }
     setActiveView(SECTIONS.LINKS);
     setActiveLink(null);
   }
@@ -20,11 +24,21 @@ export function ResumeAction({ action }: ResumeActionLinkProps) {
   return (
     <div className="pdf-link">
       <motion.h3 {...enterExitAnimationProps} onClick={viewOrDownload}>
-        {strings[action]}
+        {action === 'download' ? (
+          <PDFDownloadLink
+            document={<ResumePDF />}
+            style={{ textDecoration: 'none' }}
+            tabIndex={-1}
+          >
+            {strings[action]}
+          </PDFDownloadLink>
+        ) : (
+          <>{strings[action]}</>
+        )}
       </motion.h3>
 
       <motion.div className="links" {...enterExitAnimationProps}>
-        <ViewOrDownload action={action} onClick={viewOrDownload} />
+        <ViewOrDownload action={action} />
       </motion.div>
     </div>
   );
