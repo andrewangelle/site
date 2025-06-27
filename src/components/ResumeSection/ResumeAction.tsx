b8388a41f81cf9c2c8e6c46e6a2bf5182c9f55cb
Andrew Angelle <andrewangelle@gmail.com>
2025-06-26 20:28:03 -0400

    remove the actions on resume headings

--- src/components/ResumeSection/ResumeAction.tsx
+++ src/components/ResumeSection/ResumeAction.tsx
@@ -1,41 +1,13 @@
-import { PDFDownloadLink } from '@react-pdf/renderer';
-import { useNavigate } from '@tanstack/react-router';
-import { useSetAtom } from 'jotai';
 import { motion } from 'motion/react';
-import { ResumePDF } from '~/PDF/ResumePDF';
 import { ViewOrDownload } from '~/components/ResumeSection/ViewOrDownload';
-import { SECTIONS, activeLinkAtom, activeViewAtom } from '~/store/atoms';
 import { enterExitAnimationProps, strings } from '~/utils/constants';
 
 export type ResumeActionLinkProps = { action: 'view' | 'download' };
 
 export function ResumeAction({ action }: ResumeActionLinkProps) {
-  const navigate = useNavigate();
-  const setActiveLink = useSetAtom(activeLinkAtom);
-  const setActiveView = useSetAtom(activeViewAtom);
-  function viewOrDownload() {
-    if (action === 'view') {
-      navigate({ to: '/resume' });
-    }
-    setActiveView(SECTIONS.LINKS);
-    setActiveLink(null);
-  }
-
   return (
     <div className="pdf-link">
-      <motion.h3 {...enterExitAnimationProps} onClick={viewOrDownload}>
-        {action === 'download' ? (
-          <PDFDownloadLink
-            document={<ResumePDF />}
-            style={{ textDecoration: 'none' }}
-            tabIndex={-1}
-          >
-            {strings[action]}
-          </PDFDownloadLink>
-        ) : (
-          <>{strings[action]}</>
-        )}
-      </motion.h3>
+      <motion.h3 {...enterExitAnimationProps}>{strings[action]}</motion.h3>
 
       <motion.div className="links" {...enterExitAnimationProps}>
         <ViewOrDownload action={action} />
