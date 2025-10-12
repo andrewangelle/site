6b9f90d33ff5ecd2f885a2d059401f9d3bc03065
Andrew Angelle <andrewangelle@gmail.com>
2025-10-12 13:57:45 -0400

    Update tsr. Update netlify. Fix overscroll (#36)

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import viteReact from '@vitejs/plugin-react'
+import netlify from '@netlify/vite-plugin-tanstack-start';
 
 export default defineConfig({
   server: {
@@ -11,10 +12,8 @@ export default defineConfig({
     tsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
-    tanstackStart({
-      target: 'netlify',
-      customViteReactPlugin: true
-    }),
+    tanstackStart(),
+    netlify(),
     viteReact()
   ],
 })
