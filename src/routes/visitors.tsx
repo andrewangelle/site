e615bba068dd3dd088460902632a9ac7edfe641a
Andrew Angelle <andrewangelle@gmail.com>
2025-12-06 13:11:41 +0100

    Remove dev impl

--- src/routes/visitors.tsx
+++ src/routes/visitors.tsx
@@ -1,17 +1,20 @@
+import { getStore } from '@netlify/blobs';
 import { createFileRoute } from '@tanstack/react-router';
-import { readDev, readProd } from '~/services/visitors';
 
 export const Route = createFileRoute('/visitors')({
   server: {
     handlers: {
       async GET() {
-        const visitorsData =
-          process.env.NODE_ENV === 'development'
-            ? await readDev()
-            : await readProd();
+        const store = getStore({
+          name: 'site-store',
+          siteID: process.env.SITE_BLOB_ID,
+          token: process.env.SITE_TOKEN,
+        });
+
+        const visitors = await store.get('visitors', { type: 'json' });
 
         const data = JSON.stringify({
-          count: visitorsData.visitors.length ?? 0,
+          count: visitors.length ?? 0,
         });
 
         const options = {
@@ -19,6 +22,7 @@ export const Route = createFileRoute('/visitors')({
             'Content-Type': 'application/json',
           },
         };
+
         return new Response(data, options);
       },
     },
--- src/services/visitors/index.ts
+++ src/services/visitors/index.ts
@@ -1,62 +1,24 @@
 import crypto from 'node:crypto';
-import { existsSync } from 'node:fs';
-import { readFile, writeFile } from 'node:fs/promises';
-import path from 'node:path';
 import { getStore } from '@netlify/blobs';
 import { createMiddleware } from '@tanstack/react-start';
 
 export const visitorsMiddleware = createMiddleware().server(
   async ({ request, next }) => {
-    const fingerprint = createFingerprint(request);
-
-    if (process.env.NODE_ENV === 'development') {
-      await writeDev(fingerprint);
-    } else {
-      await writeProd(fingerprint);
-    }
-
+    await write(createFingerprint(request));
     return next();
   },
 );
 
-async function writeProd(fingerprint: string) {
-  const store = getStore('site-store');
+async function write(fingerprint: string) {
+  const store = getStore({
+    name: 'site-store',
+    siteID: process.env.SITE_BLOB_ID,
+    token: process.env.SITE_TOKEN,
+  });
   const visitorStore = await store.get('visitors', { type: 'json' });
-  const visitorData = JSON.parse(visitorStore) as string[];
+  const visitorData = JSON.parse(visitorStore) ?? ([] as string[]);
   const nextState = Array.from(new Set([...visitorData, fingerprint]));
   await store.setJSON('visitors', nextState);
-}
-
-async function writeDev(fingerprint: string) {
-  const visitorsFilePath = path.join(import.meta.dirname, './db.json');
-
-  let visitorData: { visitors: string[] } = { visitors: [] };
-
-  if (existsSync(visitorsFilePath)) {
-    const dbFileBase = await readFile(visitorsFilePath, 'utf-8');
-    visitorData = JSON.parse(dbFileBase) as { visitors: string[] };
-  }
-
-  const nextState = {
-    visitors: Array.from(new Set([...visitorData.visitors, fingerprint])),
-  };
-
-  await writeFile(visitorsFilePath, JSON.stringify(nextState));
-}
-
-export async function readDev() {
-  const visitorsFilePath = path.join(import.meta.dirname, './db.json');
-  const dbFile = await readFile(visitorsFilePath, 'utf-8');
-  return JSON.parse(dbFile) as { visitors: string[] };
-}
-
-export async function readProd() {
-  const store = getStore('site-store');
-  const visitorStore = await store.get('visitors', { type: 'json' });
-  const visitorData = JSON.parse(visitorStore) as string[];
-  return {
-    visitors: visitorData,
-  };
 }
 
 function createFingerprint(req: Request): string {
