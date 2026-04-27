2cc32d98156084610d13b72698e4ab6e3e04d951
Andrew Angelle <andrewangelle@gmail.com>
2026-04-27 09:05:31 -0400

    feat: Setup sentry

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,8 +1,19 @@
-import { Link, useNavigate } from '@tanstack/react-router';
+import * as Sentry from '@sentry/tanstackstart-react';
+import {
+  Link,
+  type NotFoundRouteProps,
+  useNavigate,
+} from '@tanstack/react-router';
+import { useEffect } from 'react';
 import { strings } from '~/utils/constants';
 
-export function NotFound() {
+export function NotFound(props: NotFoundRouteProps) {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    Sentry.captureException(props);
+  }, [props]);
+
   return (
     <div className="section">
       <div className="name-container">
