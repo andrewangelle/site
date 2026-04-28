408e62201a28d9995eebc4c9325aaf5b47503cef
Andrew Angelle <andrewangelle@gmail.com>
2026-04-28 08:44:28 -0400

    chore: add linked errors integration

--- src/router.ts
+++ src/router.ts
@@ -22,6 +22,7 @@ export function getRouter() {
       integrations: [
         Sentry.tanstackRouterBrowserTracingIntegration(router),
         Sentry.replayIntegration(),
+        Sentry.linkedErrorsIntegration(),
       ],
       tracesSampleRate: 1.0,
       replaysSessionSampleRate: 0.1,
